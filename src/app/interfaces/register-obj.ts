9511f16d263207faa89ac3625dd113e1e74eec69
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2025-02-22 17:33:34 -0500

    add interface RegisterObj

---
+++ src/app/interfaces/register-obj.ts
@@ -0,0 +1,10 @@
+export interface RegisterObj {
+    userId: number,
+    userName: string,
+    emailId: string,
+    fullName: string,
+    role: string,
+    createdDate: Date,
+    password: string,
+    projectName: string,
+}
